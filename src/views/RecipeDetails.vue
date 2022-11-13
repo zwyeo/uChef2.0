15903172f918a805421f9102f35c3eba6ccf4777
zwyeo <>
2022-11-13 21:05:56 +0800

    added login modal to bookmark

--- src/views/RecipeDetails.vue
+++ src/views/RecipeDetails.vue
@@ -7,8 +7,14 @@
     <div class="row">
       <div class="col-12">
         <div class="parent">
-          <div class="background-image" :style="{ backgroundImage: 'url(' + image + ')' }"></div>
-          <div class="hero-image" :style="{ backgroundImage: 'url(' + image + ')' }"></div>
+          <div
+            class="background-image"
+            :style="{ backgroundImage: 'url(' + image + ')' }"
+          ></div>
+          <div
+            class="hero-image"
+            :style="{ backgroundImage: 'url(' + image + ')' }"
+          ></div>
         </div>
       </div>
     </div>
@@ -24,27 +30,48 @@
             <!-- IF VIDEO EXISTS -->
             <div v-if="videoExist == true">
               <!-- video button -->
-              <button class="btn video-btn" data-bs-toggle="modal" :data-src="video" data-bs-target="#myModal"
-                @click="open()">
+              <button
+                class="btn video-btn"
+                data-bs-toggle="modal"
+                :data-src="video"
+                data-bs-target="#myModal"
+                @click="open()"
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                   <path
-                    d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
+                    d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+                  />
                 </svg>
                 Watch Video
               </button>
 
               <!-- Modal -->
-              <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
+              <div
+                class="modal fade"
+                id="myModal"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
                 <div class="modal-dialog" role="document">
                   <div class="modal-content">
                     <div class="modal-body">
-                      <button type="button" class="btn-close" @click="close()" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        @click="close()"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
                       <!-- 16:9 aspect ratio -->
                       <div class="ratio ratio-16x9">
-                        <iframe class="embed-responsive-item" :src="video2" allowscriptaccess="always"
-                          allow="autoplay"></iframe>
+                        <iframe
+                          class="embed-responsive-item"
+                          :src="video2"
+                          allowscriptaccess="always"
+                          allow="autoplay"
+                        ></iframe>
                       </div>
                     </div>
                   </div>
@@ -60,7 +87,9 @@
 
         <div class="col-12 col-md-4">
           <div class="receipe-ratings my-5 float-end">
-            <div class="ratings animate__animated animate__delay-1s animate__pulse animate__fast">
+            <div
+              class="ratings animate__animated animate__delay-1s animate__pulse animate__fast"
+            >
               <div v-if="ratings == null || ratings == 4">
                 <i class="fa fa-star" aria-hidden="true"></i>
                 <i class="fa fa-star" aria-hidden="true"></i>
@@ -110,23 +139,48 @@
               </div>
             </div>
 
-            <a v-if="!bookmarked" href="#"
+            <a
+              v-if="!bookmarked && !this.$store.state.userId"
+              href="#"
               class="btn delicious-btn animate__animated animate__rubberBand animate__delay-1s animate__fast"
-              @click="bookmark()" data-bs-toggle="modal" data-bs-target="#bookmarkModal">Bookmark</a>
-            <a v-else href="#"
+              data-bs-toggle="modal"
+              data-bs-target="#loginModal"
+              >Bookmark</a
+            >
+            <a
+              v-if="!bookmarked && this.$store.state.userId"
+              href="#"
               class="btn delicious-btn animate__animated animate__rubberBand animate__delay-1s animate__fast"
-              @click="unbookmark()" data-bs-toggle="modal" data-bs-target="#unbookmarkModal">Bookmarked!</a>
+              @click="bookmark()"
+              data-bs-toggle="modal"
+              data-bs-target="#bookmarkModal"
+              >Bookmark</a
+            >
+            <a
+              v-if="bookmarked && this.$store.state.userId"
+              href="#"
+              class="btn delicious-btn animate__animated animate__rubberBand animate__delay-1s animate__fast"
+              @click="unbookmark()"
+              data-bs-toggle="modal"
+              data-bs-target="#unbookmarkModal"
+              >Bookmarked!</a
+            >
           </div>
         </div>
       </div>
 
       <!-- Instructions -->
       <div class="row">
-        <div class="col-12 col-lg-9 animate__animated animate__backInUp animate__delay-1s animate__fast">
+        <div
+          class="col-12 col-lg-9 animate__animated animate__backInUp animate__delay-1s animate__fast"
+        >
           <!-- Single Preparation Step -->
           <ul class="list-group list-group-light">
-            <li v-for="(item, index) in instructions" :key="item"
-              class="list-group-item px-3 border-0 rounded-3 list-group-item-light mb-2 d-flex">
+            <li
+              v-for="(item, index) in instructions"
+              :key="item"
+              class="list-group-item px-3 border-0 rounded-3 list-group-item-light mb-2 d-flex"
+            >
               <h4 v-if="index < 9">0{{ index + 1 }}</h4>
               <h4 v-else>{{ index + 1 }}</h4>
               <p class="ms-3 instruction-text">{{ item }}</p>
@@ -135,15 +189,26 @@
         </div>
 
         <!-- Ingredients -->
-        <div class="col-12 col-lg-3 animate__animated animate__backInRight animate__delay-1s animate__fast">
+        <div
+          class="col-12 col-lg-3 animate__animated animate__backInRight animate__delay-1s animate__fast"
+        >
           <h4 class="">Ingredients</h4>
 
           <!-- Ingredient list -->
-          <div v-for="(item, index) in ingredient_list" :key="index" :id="index">
+          <div
+            v-for="(item, index) in ingredient_list"
+            :key="index"
+            :id="index"
+          >
             <ul class="list-group text-white ingredient-group">
-              <li class="list-group-item d-flex justify-content-between align-content-center">
+              <li
+                class="list-group-item d-flex justify-content-between align-content-center"
+              >
                 <h6 class="mb-0">
-                  <img src="../assets/img/core-img/salad.png" class="ingredient-img" />
+                  <img
+                    src="../assets/img/core-img/salad.png"
+                    class="ingredient-img"
+                  />
                   &nbsp;<span class="item">{{ item }}</span>
                 </h6>
               </li>
@@ -158,19 +223,31 @@
         <div class="col-12">
           <div
             class="section-heading text-left m-0 animate__animated animate__fadeInBottomLeft animate__delay-1s animate__fast"
-            id="review-section">
+            id="review-section"
+          >
             <h3>Reviews</h3>
             <p>See what others have to say</p>
           </div>
-          <div v-if="review_list.length > 0"
-            class="animate__bounceInUp animate__animated animate__delay-1s animate__fast">
-            <review-card v-for="(review, index) of review_list" :key="index" :id="id" :user="review.user"
-              :date="review.date" :rating="review.rating" :subject="review.subject" :message="review.message">
+          <div
+            v-if="review_list.length > 0"
+            class="animate__bounceInUp animate__animated animate__delay-1s animate__fast"
+          >
+            <review-card
+              v-for="(review, index) of review_list"
+              :key="index"
+              :id="id"
+              :user="review.user"
+              :date="review.date"
+              :rating="review.rating"
+              :subject="review.subject"
+              :message="review.message"
+            >
             </review-card>
           </div>
           <div v-else>
             <p
-              class="text-center pt-5 pb-5 animate__animated animate__fadeInBottomLeft animate__delay-1s animate__fast">
+              class="text-center pt-5 pb-5 animate__animated animate__fadeInBottomLeft animate__delay-1s animate__fast"
+            >
               No reviews yet.
             </p>
           </div>
@@ -182,21 +259,73 @@
   <review-btn></review-btn>
   <!-- ##### Follow Us Instagram Area End ##### -->
 
+  <!-- Login Modal -->
+  <div
+    class="modal fade"
+    id="loginModal"
+    tabindex="-1"
+    aria-labelledby="ModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5 mx-auto" id="exampleModalLabel">
+            Login is required!
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body m-3 mx-auto">
+          <img src="../assets/img/core-img/notLoggedIn.png" alt="" />
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-success mx-auto"
+            data-bs-dismiss="modal"
+          >
+            Ok
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <!-- Bookmark Modal -->
-  <div class="modal fade" id="bookmarkModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+  <div
+    class="modal fade"
+    id="bookmarkModal"
+    tabindex="-1"
+    aria-labelledby="ModalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5 mx-auto" id="exampleModalLabel">
             Item bookmarked! 🥳
           </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body m-3 mx-auto">
           Awesome! {{ title }} will be in your bookmark page!
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-success mx-auto" data-bs-dismiss="modal">
+          <button
+            type="button"
+            class="btn btn-success mx-auto"
+            data-bs-dismiss="modal"
+          >
             Ok
           </button>
         </div>
@@ -205,20 +334,35 @@
   </div>
 
   <!-- Unbookmark Modal -->
-  <div class="modal fade" id="unbookmarkModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+  <div
+    class="modal fade"
+    id="unbookmarkModal"
+    tabindex="-1"
+    aria-labelledby="ModalLabel"
+    aria-hidden="true"
+  >
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5 mx-auto" id="exampleModalLabel">
             Awww 😥... Item unbookmarked!
           </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body m-3 mx-auto">
           {{ title }} will be removed your bookmark page!
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-success mx-auto" data-bs-dismiss="modal">
+          <button
+            type="button"
+            class="btn btn-success mx-auto"
+            data-bs-dismiss="modal"
+          >
             Ok
           </button>
         </div>
@@ -320,10 +464,10 @@ export default {
           this.title = obj.strMeal;
           this.image = obj.strMealThumb;
           this.foodCategory = obj.strCategory;
-          console.log(obj.strYoutube)
+          console.log(obj.strYoutube);
           if (obj.strYoutube === "") {
             this.videoExist = false;
-            console.log(this.videoExist)
+            console.log(this.videoExist);
           }
           this.video = obj.strYoutube.replace("watch?v=", "embed/");
 
@@ -589,7 +733,6 @@ ol li {
   background-position: center;
 }
 
-
 /* Section Heading */
 .section-heading {
   position: relative;
@@ -746,7 +889,6 @@ ol li {
 }
 
 @media only screen and (max-width: 767px) {
-
   .delicious-btn.btn-4.active,
   .delicious-btn.btn-4:hover,
   .delicious-btn.btn-4:focus {
@@ -834,7 +976,6 @@ svg {
   font-size: 20px;
 }
 
-
 /* Ingredient list */
 .ingredient-group {
   width: 300px !important;
@@ -853,7 +994,6 @@ svg {
   transform: scaleX(1.1);
 }
 
-
 .list-group-item:hover .check {
   opacity: 1;
 }
@@ -868,7 +1008,6 @@ svg {
   top: 20px;
   left: 80px;
 }
-
 
 /* Video Modal */
 .modal-dialog {
